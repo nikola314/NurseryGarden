ca0b70ed4299b156d67d1167f0278106c0c85f4d
Nikola Kesic <nikola.kesic97@gmail.com>
2020-04-21 01:03:03 +0200

    changed user model on backend

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String },
+    approved: { type: Boolean, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    phone: { type: String },
+    location: { type: String, required: true },
+    date: { type: Date, required: true },
+    isCompany: { type: Boolean, required: true }
 });
 
 userSchema.plugin(uniqueValidator);
